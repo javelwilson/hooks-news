7659e6a75cd85c7da4eaa8f162e0cdbcfad15a1a
Javel Wilson <javelawilson@gmail.com>
2020-09-19 01:08:28 -0500

    Add dependencies and create components

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hooks news', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = getByText(/hooks news/i);
+  expect(h1Element).toBeInTheDocument();
 });
---
+++ src/components/Link/SearchLinks.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SearchLinks = () => {
+  return <div>SearchLinks</div>
+}
+
+export default SearchLinks
